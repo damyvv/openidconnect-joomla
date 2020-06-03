ab9a81088c9dedf48375c8540b05c74bff7e0bbf
Damy van Valenberg <damyvv@hotmail.com>
2020-06-03 20:33:52 +0200

    Created controller

---
+++ com_openidconnect/site/controller.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_openidconnect
+ *
+ * @copyright   2020 Coval B.V.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * OpenID Connect Component Controller
+ *
+ * @since  0.0.1
+ */
+class OpenIDConnectController extends JControllerLegacy
+{
+    function display() {
+        die('Display task');
+    }
+    
+    function test() {
+        die('test');
+    }
+}
--- com_openidconnect/site/openidconnect.php
+++ com_openidconnect/site/openidconnect.php
@@ -1,1 +1,21 @@
-Hello from the OpenID Connect component.
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_openidconnect
+ *
+ * @copyright   2020 Coval B.V.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Get an instance of the controller prefixed by OpenIDConnect
+$controller = JControllerLegacy::getInstance('OpenIDConnect');
+
+// Perform the Request task
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
